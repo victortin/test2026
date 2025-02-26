04157eda13ac50009720c3f0fbda7d68a58f7b7d
Victor Ting <victor.tin@gmail.com>
2025-02-26 19:52:10 +0800

    初始化

---
+++ test202633/src/main/java/net/ting/Test202633Application.java
@@ -0,0 +1,13 @@
+package net.ting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test202633Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test202633Application.class, args);
+	}
+
+}
---
+++ test202633/src/test/java/net/ting/Test202633ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test202633ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
